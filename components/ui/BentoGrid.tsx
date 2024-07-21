cd22d5c3d392d5d0afff8aae93cdc9cb14b412e0
jesus-rios <jesus.rios@atventurepartners.tech>
2024-07-21 01:02:41 -0700

    grabbed another component from aceternityUI which is an interative globe and implementing it on grid item 2

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { Globe } from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -95,6 +96,8 @@ export const BentoGridItem = ({
             {title}
           </div>
         </div>
+
+        {id === 2 && <Globe />}
       </div>
     </div>
   );

64ff03cc3954a615c13b1aa7ec0bcd58a0c30f53
jesus-rios <jesus.rios@atventurepartners.tech>
2024-07-23 01:07:29 -0700

    worked on adding an email address copy button and did more styling on grid items for better placement

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,8 +1,12 @@
+"use client";
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
-import { Globe } from "./Globe";
 import { GridGlobe } from "./GridGlobe";
-import { div } from "three/examples/jsm/nodes/Nodes.js";
+import Lottie from "react-lottie";
+import { useState } from "react";
+import animationData from "@/data/confetti.json";
+import MagicButton from "./MagicButton";
+import { IoCopyOutline } from "react-icons/io5";
 
 export const BentoGrid = ({
   className,
@@ -44,10 +48,18 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText("jesusfernandorios@outlook.com");
+
+    setCopied(true);
+  };
+
   return (
     <div
       className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]",
         className
       )}
       style={{
@@ -56,7 +68,7 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(32,30,33,1) 24%, rgba(139,92,171,1) 80%)",
       }}
     >
-      <div className={`${id === 6 && "flex justify-center h-full"} `}>
+      <div className={`${id === 6 && "flex justify-center"} h-full `}>
         <div className="w-full h-full absolute">
           {img && (
             <img
@@ -114,6 +126,7 @@ export const BentoGridItem = ({
                 <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
               </div>
               <div className="flex flex-col gap-3 lg:gap-8">
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
                 {["NodeJS", "Axios", "MongoDB"].map((item) => (
                   <span
                     key={item}
@@ -122,8 +135,32 @@ export const BentoGridItem = ({
                     {item}
                   </span>
                 ))}
-                <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
               </div>
+            </div>
+          )}
+
+          {id === 6 && (
+            <div className="mt-5 relative">
+              {" "}
+              <div className={`absolute -bottom-5 right-0 `}>
+                <Lottie
+                  options={{
+                    loop: copied,
+                    autoplay: copied,
+                    animationData,
+                    rendererSettings: {
+                      preserveAspectRatio: "xMidYMid slice",
+                    },
+                  }}
+                />
+              </div>
+              <MagicButton
+                title={copied ? "Email Copied!" : "Copy My Email"}
+                icon={<IoCopyOutline />}
+                position="left"
+                otherClasses="!bg-[#161831]"
+                handleClick={handleCopy}
+              />
             </div>
           )}
         </div>

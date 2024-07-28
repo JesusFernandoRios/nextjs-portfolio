768911b604916ed0abb5d37a941a50511c16426b
jesus-rios <jesus.rios@atventurepartners.tech>
2024-07-28 11:54:54 -0700

    started work on the projects part of the app. buildiing project cards

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,5 +1,6 @@
 import { projects } from "@/data";
 import React from "react";
+import { PinContainer } from "./ui/3d-Pin";
 
 const RecentProjects = () => {
   return (
@@ -11,10 +12,25 @@ const RecentProjects = () => {
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw] lg:h-[30vh] mb-10"
             key={id}
           >
-            {title}
+            <PinContainer title={title} href={link}>
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh]">
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
+                  <img src="/bg.png" alt="bg image" />
+                </div>
+                <img src={img} alt="cover" className="z-10 absolute bottom-0" />
+              </div>
+
+              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                {title}
+              </h1>
+
+              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+                {des}
+              </p>
+            </PinContainer>
           </div>
         ))}
       </div>
